ba2f1e1202f2ce2806ebdacd1407cc122d205a44
ybstring <76188896+ybstring@users.noreply.github.com>
2022-03-10 20:21:39 -0500

    Update PluginProcessor.cpp

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -95,9 +95,6 @@ void SlapbackDelayAudioProcessor::changeProgramName (int index, const juce::Stri
 //==============================================================================
 void SlapbackDelayAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
-
     const int numInputChannels = getNumInputChannels();
     const int delayBufferSize = 2 * (sampleRate + samplesPerBlock);
     mSampleRate = sampleRate;
@@ -143,29 +140,14 @@ void SlapbackDelayAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
+         buffer.clear (i, 0, buffer.getNumSamples());
 
     const int bufferLength = buffer.getNumSamples();
     const int delayBufferLength = mDelayBuffer.getNumSamples();
 
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
-        // ..do something to the data...
-
         const float* bufferData = buffer.getReadPointer(channel);
         const float* delayBufferData = mDelayBuffer.getReadPointer(channel);
 
