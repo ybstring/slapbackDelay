0a5ce6ff64629e2d47f0fde6a847a702a43b4a24
ybstring <76188896+ybstring@users.noreply.github.com>
2022-03-10 20:18:22 -0500

    Update PluginEditor.h

--- PluginEditor.h
+++ PluginEditor.h
@@ -26,9 +26,6 @@ public:
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-
     void sliderValueChanged(juce::Slider* slider) override;
     SlapbackDelayAudioProcessor& audioProcessor;
     juce::Slider delayControl;
